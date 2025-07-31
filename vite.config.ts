07d5bd0aac2f9d65aeb35175f5819991404eeaa4
jicoim <jicoim@iu.edu>
2025-07-30 22:13:27 -0700

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/jithenthiriya-portfolio/",
   server: {
     host: "::",
     port: 8080,
